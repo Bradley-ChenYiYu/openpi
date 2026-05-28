e6947aad3553ea237e41207634ac44f2ac846605
Bradley-ChenYiYu <172489412+Bradley-ChenYiYu@users.noreply.github.com>
2026-05-28 10:41:01 +0000

    Train front on new data20250528

--- scripts/run_training_from_raw_data.sh
+++ scripts/run_training_from_raw_data.sh
@@ -21,10 +21,10 @@ START_STEP="${START_STEP:-3}"
 
 # ===== Path Variables =====
 CONFIG_NAME="pi0_tracer_front_finetune"
-METADATA_CONFIG="scripts/rosbag-to-lerobot/config/tracer_side_views_metadata_20260527.yaml"
+METADATA_CONFIG="scripts/rosbag-to-lerobot/config/tracer_side_views_metadata_20260528.yaml"
 TOPIC_MAPPING_CONFIG="scripts/rosbag-to-lerobot/config/tracer_side_views_topic_mapping.yaml"
 ROSBAG_DIRS=(
-    "rosbag_dir/rosbag_dir_20260527"
+    "rosbag_dir/rosbag_dir_20260528"
 )
 ROSBAG2VIDEO_RATE="50"
 
@@ -34,7 +34,7 @@ VID_PROMPT_METADATA_PATH="$METADATA_CONFIG"
 
 # ===== convert_rosbag_to_lerobot.py Variables =====
 CONVERT_INPUT_BAG_PATHS=("${ROSBAG_DIRS[@]}")
-CONVERT_REPO_ID="brad/tracer_data_side_views_20260527"
+CONVERT_REPO_ID="brad/tracer_data_side_views_20260528"
 CONVERT_ROBOT_TYPE="tracer"
 CONVERT_FPS="50"
 CONVERT_CONFIG_PATH="$TOPIC_MAPPING_CONFIG"
@@ -46,7 +46,7 @@ COMPUTE_NORM_CONFIG_NAME="$CONFIG_NAME"
 
 # ===== train.py Variables =====
 TRAIN_CONFIG_NAME="$CONFIG_NAME"
-TRAIN_EXP_NAME="front_cam_dinning_data20260527_$(date +%Y%m%d_%H%M%S)"
+TRAIN_EXP_NAME="front_cam_dinning_data20260528_$(date +%Y%m%d_%H%M%S)"
 TRAIN_OVERWRITE_FLAG="--overwrite"
 TRAIN_XLA_MEM_FRACTION="0.9"
 TRAIN_OUTPUT_LOG="train_output_${TRAIN_EXP_NAME}.log"
